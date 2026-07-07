369138cc26e2ac19c2d2de71328e23e36a68fc05
plebey <mixail.card@gmail.com>
2026-07-07 12:58:38 +0300

    add realisation for local recipe repo (List storage)

---
+++ RecipeBox/Repository/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,16 @@
+﻿using RecipeBox.Models;
+
+namespace RecipeBox.Repository.Interfaces
+{
+    public interface IRecipeRepository
+    {
+        //GetAll, GetById, Create, Update, Delete, GetByName
+        IEnumerable<Recipe> GetAll();
+        Recipe? GetById(int id);
+        Recipe? Create(Recipe recipe);
+        bool Update(int id, Recipe recipe);
+        bool Delete(int id);
+        IEnumerable<Recipe> GetByName(string name);
+
+    }
+}
---
+++ RecipeBox/Repository/LocalRecipeRepository.cs
@@ -0,0 +1,76 @@
+﻿using RecipeBox.Models;
+using RecipeBox.Repository.Interfaces;
+using System.Linq.Expressions;
+
+namespace RecipeBox.Repository
+{
+    public class LocalRecipeRepository: IRecipeRepository
+    {
+        private int _next_id = 1;
+        private List<Recipe> _recipes = new List<Recipe>();
+
+        public LocalRecipeRepository() 
+        {
+
+        }
+
+        public IEnumerable<Recipe> GetAll()
+        {
+            return _recipes;
+        }
+
+        public Recipe? GetById(int id)
+        {
+            return _recipes.Where(rec => rec.Id == id).FirstOrDefault();
+        }
+
+        public Recipe? Create(Recipe recipe)
+        {
+            recipe.Id = _next_id;
+            _recipes.Add(recipe);
+            _next_id++;
+
+            return recipe;
+        }
+
+        public bool Update(int id, Recipe recipe)
+        {
+            try
+            {
+                int index = _recipes.FindIndex(r => r.Id == id);
+                if (index == -1)
+                    return false; // не нашли
+
+                recipe.Id = _recipes[index].Id;
+                _recipes[index] = recipe;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                int index = _recipes.FindIndex(rec => rec.Id == id);
+                if (index == -1)
+                    return false;
+                _recipes.RemoveAt(index);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+            
+        public IEnumerable<Recipe> GetByName(string name)
+        {
+            return _recipes.Where(rec => rec.Name == name).ToList();
+        }
+    }
+}
