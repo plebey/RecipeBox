84276c51e56010831ca3d3f2ec769a8275dc10e0
plebey <mixail.card@gmail.com>
2026-07-07 12:59:11 +0300

    add recipe service with interface

---
+++ RecipeBox/Services/Interfaces/IRecipeService.cs
@@ -0,0 +1,14 @@
+﻿using RecipeBox.Models;
+
+namespace RecipeBox.Services.Interfaces
+{
+    public interface IRecipeService
+    {
+        IEnumerable<Recipe> GetAll();
+        Recipe? GetById(int id);
+        Recipe? Create(Recipe recipe);
+        bool Update(int id, Recipe recipe);
+        bool Delete(int id);
+        IEnumerable<Recipe> GetByName(string name);
+    }
+}
---
+++ RecipeBox/Services/RecipeService.cs
@@ -0,0 +1,49 @@
+﻿using RecipeBox.Models;
+using RecipeBox.Repository.Interfaces;
+using RecipeBox.Services.Interfaces;
+
+namespace RecipeBox.Services
+{
+    public class RecipeService: IRecipeService
+    {
+        private readonly IRecipeRepository _repository;
+
+        public RecipeService(IRecipeRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public IEnumerable<Recipe> GetAll()
+        {
+            return _repository.GetAll();
+        }
+        public Recipe? GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+        public Recipe? Create(Recipe recipe)
+        {
+            if (recipe == null)
+            {
+                return null;
+            }
+            if (string.IsNullOrWhiteSpace(recipe.Name))
+            {
+                return null;
+            }
+            return _repository.Create(recipe);
+        }
+        public bool Update(int id, Recipe recipe)
+        {
+            return _repository.Update(id, recipe);
+        }
+        public bool Delete(int id)
+        {
+            return _repository.Delete(id);
+        }
+        public IEnumerable<Recipe> GetByName(string name)
+        {
+            return _repository.GetByName(name);
+        }
+    }
+}
