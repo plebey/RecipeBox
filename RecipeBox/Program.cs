8e68b12a91b408419bf7968995318b722f726f90
plebey <mixail.card@gmail.com>
2026-07-07 13:00:12 +0300

    add services in builder and connect scalar

--- RecipeBox/Program.cs
+++ RecipeBox/Program.cs
@@ -1,9 +1,19 @@
+using RecipeBox.Repository;
+using RecipeBox.Repository.Interfaces;
+using RecipeBox.Services;
+using RecipeBox.Services.Interfaces;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddSingleton<IRecipeRepository, LocalRecipeRepository>();
+builder.Services.AddScoped<IRecipeService, RecipeService>();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,6 +22,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
