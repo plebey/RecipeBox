5cc40b812c7379c60439b78d8d9f308ccb9eec61
plebey <mixail.card@gmail.com>
2026-08-04 18:42:01 +0300

    add global exception handler and file logging

---
+++ RecipeBox/ExceptionHandle/GlobalExceptionHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace RecipeBox.ExceptionHandle
+{
+    public class GlobalExceptionHandler: IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext httpContext,
+            Exception exception,
+            CancellationToken cancellationToken)
+        {
+            _logger.LogError(
+                exception,
+                "Unhandled exception occurred.");
+
+            httpContext.Response.StatusCode =
+                StatusCodes.Status500InternalServerError;
+
+            await httpContext.Response.WriteAsJsonAsync(
+                new
+                {
+                    status = 500,
+                    title = "Internal server error",
+                    detail = "An unexpected error occurred."
+                },
+                cancellationToken);
+
+            return true;
+        }
+    }
+}
--- RecipeBox/Program.cs
+++ RecipeBox/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using RecipeBox.Data;
+using RecipeBox.ExceptionHandle;
 using RecipeBox.Repository;
 using RecipeBox.Repository.Interfaces;
 using RecipeBox.Services;
 using RecipeBox.Services.Interfaces;
 using Scalar.AspNetCore;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +23,21 @@ builder.Services.AddScoped<IRecipeRepository, SQLRecipeRepository>();
 builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
 builder.Services.AddDbContext<DBContextRecipeBox>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddSerilog(configuration =>
+{
+    configuration
+        .MinimumLevel.Information()
+        .WriteTo.Console()
+        .WriteTo.File(
+            "Logs/log-.txt",
+            rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: 14,
+            restrictedToMinimumLevel: LogEventLevel.Error);
+});
+
+builder.Services.AddProblemDetails();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -30,6 +48,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
 
